2b01031d87990bb3f304f6c0b477c131f127f895
Daniel Martí <mvdan@mvdan.cc>
2015-02-23 12:14:02 +0100

    Add comments to each PseudoTLDs item

--- pseudotlds.go
+++ pseudotlds.go
@@ -2,9 +2,9 @@ package xurls
 
 // PseudoTLDs is a sorted list of some widely used unofficial TLDs
 var PseudoTLDs = []string{
-	`bit`,
-	`exit`,
-	`i2p`,
-	`local`,
-	`onion`,
+	`bit`,   // Namecoin
+	`exit`,  // Tor exit node
+	`i2p`,   // I2P network
+	`local`, // Local network
+	`onion`, // Tor hidden services
 }
