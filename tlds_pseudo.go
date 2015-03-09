d81d8cb9c156d262256b91de5ce320a827d34062
Daniel Martí <mvdan@mvdan.cc>
2015-03-09 15:29:02 +0100

    Add sources for pseudo-tlds

--- tlds_pseudo.go
+++ tlds_pseudo.go
@@ -1,6 +1,10 @@
 package xurls
 
 // PseudoTLDs is a sorted list of some widely used unofficial TLDs
+//
+// Sources:
+//  * https://en.wikipedia.org/wiki/Pseudo-top-level_domain
+//  * https://en.wikipedia.org/wiki/Category:Pseudo-top-level_domains
 var PseudoTLDs = []string{
 	`bit`,   // Namecoin
 	`exit`,  // Tor exit node
