0374a020541a1081f671fe9f097a9b30bfcf8196
Daniel Martí <mvdan@mvdan.cc>
2015-03-25 13:52:18 +0100

    Isolate pathChar

--- xurls.go
+++ xurls.go
@@ -13,6 +13,7 @@ import (
 const (
 	letters  = "a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF"
 	iriChar  = letters + `0-9`
+	pathChar = iriChar + `.,:;\-+_()?@&=$~!*%'"`
 	ipv4Addr = `(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])`
 	ipv6Addr = `([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{0,4}|:[0-9A-Fa-f]{1,4})?|(:[0-9A-Fa-f]{1,4}){0,2})|(:[0-9A-Fa-f]{1,4}){0,3})|(:[0-9A-Fa-f]{1,4}){0,4})|:(:[0-9A-Fa-f]{1,4}){0,5})((:[0-9A-Fa-f]{1,4}){2}|:(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3})|(([0-9A-Fa-f]{1,4}:){1,6}|:):[0-9A-Fa-f]{0,4}|([0-9A-Fa-f]{1,4}:){7}:`
 	ipAddr   = `(` + ipv4Addr + `|` + ipv6Addr + `)`
@@ -20,7 +21,7 @@ const (
 	hostName = `((` + iri + `\.)+` + gtld + `|` + ipAddr + `|localhost)`
 	// TODO: replacing `{0,100}` by `*` currently breaks the regex since
 	// matching is done by whichever matches first when backtracking
-	webURL   = `((https?://)?(` + hostName + `)(:[0-9]{1,5})?)(/([` + iriChar + `.,:;\-+_()?@&=$#~!*%'"]*[` + iriChar + `]|[` + iriChar + `.,:;\-+_()?@&=$#~!*%'"]{0,100}))*`
+	webURL   = `((https?://)?(` + hostName + `)(:[0-9]{1,5})?)(/([` + pathChar + `]*[` + iriChar + `]|[` + pathChar + `]{0,100}))*`
 	email    = `[a-zA-Z0-9._%\-+]{1,256}@` + hostName
 	all      = `(` + webURL + `|` + email + `)`
 )
