badd481ffc32f7eb443e35bba80b45f6febd5c21
Daniel Martí <mvdan@mvdan.cc>
2015-03-25 18:57:49 +0100

    Simplify path matching and add some tests

--- xurls.go
+++ xurls.go
@@ -11,15 +11,14 @@ import "regexp"
 const (
 	letters   = "a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF"
 	iriChar   = letters + `0-9`
-	pathChar  = iriChar + `.,:;\-+_()?@&=$~!*%'"`
+	pathChar  = iriChar + `.,:;/\-+_()?@&=$~!*%'"`
 	ipv4Addr  = `(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])`
 	ipv6Addr  = `([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:[0-9a-fA-F]{0,4}|:[0-9a-fA-F]{1,4})?|(:[0-9a-fA-F]{1,4}){0,2})|(:[0-9a-fA-F]{1,4}){0,3})|(:[0-9a-fA-F]{1,4}){0,4})|:(:[0-9a-fA-F]{1,4}){0,5})((:[0-9a-fA-F]{1,4}){2}|:(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3})|(([0-9a-fA-F]{1,4}:){1,6}|:):[0-9a-fA-F]{0,4}|([0-9a-fA-F]{1,4}:){7}:`
 	ipAddr    = `(` + ipv4Addr + `|` + ipv6Addr + `)`
 	iri       = `[` + iriChar + `]([` + iriChar + `\-]{0,61}[` + iriChar + `])?`
 	hostName  = `((` + iri + `\.)+` + gtld + `|` + ipAddr + `|localhost)`
-	nonParen  = iriChar + `.,:;\-+_?@&=$~!*%'"`
-	wellParen = `([` + nonParen + `]*(\([` + nonParen + `]*\))+)+`
-	path      = `(/(` + wellParen + `|[` + pathChar + `]*[` + iriChar + `])?)*`
+	wellParen = `([` + pathChar + `]*(\([` + pathChar + `]*\))+)+`
+	path      = `(/(` + wellParen + `|[` + pathChar + `]*[` + iriChar + `/])?)?`
 	webURL    = `(https?://)?` + hostName + `(:[0-9]{1,5})?` + path
 	email     = `[a-zA-Z0-9._%\-+]{1,256}@` + hostName
 	all       = webURL + `|` + email
