69dc8d48189cd7f4bc1eefb6a198a705ed41076f
Daniel Martí <mvdan@mvdan.cc>
2015-03-25 18:10:47 +0100

    Remove another couple of unnecessary parenthesis

--- xurls.go
+++ xurls.go
@@ -22,7 +22,7 @@ const (
 	nonParen  = iriChar + `.,:;\-+_?@&=$~!*%'"`
 	wellParen = `([` + nonParen + `]*(\([` + nonParen + `]*\))+)+`
 	path      = `(/(` + wellParen + `|[` + pathChar + `]*[` + iriChar + `])?)*`
-	webURL    = `(https?://)?(` + hostName + `)(:[0-9]{1,5})?` + path
+	webURL    = `(https?://)?` + hostName + `(:[0-9]{1,5})?` + path
 	email     = `[a-zA-Z0-9._%\-+]{1,256}@` + hostName
 	all       = webURL + `|` + email
 )
