d9678c522fab6d4524594f57f93f0e2c7cf616d8
Daniel Martí <mvdan@mvdan.cc>
2015-03-25 16:33:41 +0100

    Add proper support for well parenthesized paths
    No nested levels for now. Made regexes match the longest expression to avoid
    conflicts between this rule and the rule stating that matching parenthesis can
    be in the middle of the string.

--- xurls.go
+++ xurls.go
@@ -11,20 +11,20 @@ import (
 //go:generate go run tools/regexgen/main.go
 
 const (
-	letters  = "a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF"
-	iriChar  = letters + `0-9`
-	pathChar = iriChar + `.,:;\-+_()?@&=$~!*%'"`
-	ipv4Addr = `(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])`
-	ipv6Addr = `([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{0,4}|:[0-9A-Fa-f]{1,4})?|(:[0-9A-Fa-f]{1,4}){0,2})|(:[0-9A-Fa-f]{1,4}){0,3})|(:[0-9A-Fa-f]{1,4}){0,4})|:(:[0-9A-Fa-f]{1,4}){0,5})((:[0-9A-Fa-f]{1,4}){2}|:(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3})|(([0-9A-Fa-f]{1,4}:){1,6}|:):[0-9A-Fa-f]{0,4}|([0-9A-Fa-f]{1,4}:){7}:`
-	ipAddr   = `(` + ipv4Addr + `|` + ipv6Addr + `)`
-	iri      = `[` + iriChar + `]([` + iriChar + `\-]{0,61}[` + iriChar + `])?`
-	hostName = `((` + iri + `\.)+` + gtld + `|` + ipAddr + `|localhost)`
-	// TODO: replacing `{0,100}` by `*` currently breaks the regex since
-	// matching is done by whichever matches first when backtracking
-	path     = `(/([` + pathChar + `]{0,100}[` + iriChar + `]|[` + pathChar + `]*))*`
-	webURL   = `((https?://)?(` + hostName + `)(:[0-9]{1,5})?)` + path
-	email    = `[a-zA-Z0-9._%\-+]{1,256}@` + hostName
-	all      = `(` + webURL + `|` + email + `)`
+	letters   = "a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF"
+	iriChar   = letters + `0-9`
+	pathChar  = iriChar + `.,:;\-+_()?@&=$~!*%'"`
+	ipv4Addr  = `(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])`
+	ipv6Addr  = `([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:([0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{0,4}|:[0-9A-Fa-f]{1,4})?|(:[0-9A-Fa-f]{1,4}){0,2})|(:[0-9A-Fa-f]{1,4}){0,3})|(:[0-9A-Fa-f]{1,4}){0,4})|:(:[0-9A-Fa-f]{1,4}){0,5})((:[0-9A-Fa-f]{1,4}){2}|:(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3})|(([0-9A-Fa-f]{1,4}:){1,6}|:):[0-9A-Fa-f]{0,4}|([0-9A-Fa-f]{1,4}:){7}:`
+	ipAddr    = `(` + ipv4Addr + `|` + ipv6Addr + `)`
+	iri       = `[` + iriChar + `]([` + iriChar + `\-]{0,61}[` + iriChar + `])?`
+	hostName  = `((` + iri + `\.)+` + gtld + `|` + ipAddr + `|localhost)`
+	nonParen  = iriChar + `.,:;\-+_?@&=$~!*%'"`
+	wellParen = `([` + nonParen + `]*(\([` + nonParen + `]*\))+)+`
+	path      = `(/(` + wellParen + `|[` + pathChar + `]*[` + iriChar + `])?)*`
+	webURL    = `((https?://)?(` + hostName + `)(:[0-9]{1,5})?)` + path
+	email     = `[a-zA-Z0-9._%\-+]{1,256}@` + hostName
+	all       = `(` + webURL + `|` + email + `)`
 )
 
 // Regex expressions that match various kinds of urls and addresses
@@ -33,3 +33,9 @@ var (
 	Email  = regexp.MustCompile(email)
 	All    = regexp.MustCompile(all)
 )
+
+func init() {
+	WebURL.Longest()
+	Email.Longest()
+	All.Longest()
+}
