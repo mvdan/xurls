450264368c6ba21c8b6cf661d34edc5184a5d8c0
Daniel Martí <mvdan@mvdan.cc>
2015-04-09 14:30:06 +0200

    Separate domain regex from hostname

--- xurls.go
+++ xurls.go
@@ -17,7 +17,8 @@ const (
 	ipv6Addr  = `([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:([0-9a-fA-F]{1,4}:[0-9a-fA-F]{0,4}|:[0-9a-fA-F]{1,4})?|(:[0-9a-fA-F]{1,4}){0,2})|(:[0-9a-fA-F]{1,4}){0,3})|(:[0-9a-fA-F]{1,4}){0,4})|:(:[0-9a-fA-F]{1,4}){0,5})((:[0-9a-fA-F]{1,4}){2}|:(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3})|(([0-9a-fA-F]{1,4}:){1,6}|:):[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){7}:`
 	ipAddr    = `(` + ipv4Addr + `|` + ipv6Addr + `)`
 	iri       = `[` + iriChar + `]([` + iriChar + `\-]*[` + iriChar + `])?`
-	hostName  = `((` + iri + `\.)+` + gtld + `|` + ipAddr + `)`
+	domain    = `(` + iri + `\.)+`
+	hostName  = `(` + domain + gtld + `|` + ipAddr + `)`
 	wellParen = `([` + pathChar + `]*(\([` + pathChar + `]*\))+)+`
 	pathCont  = `(` + wellParen + `|[` + pathChar + `]*[` + endChar + `])`
 	path      = `(/` + pathCont + `?)?`
