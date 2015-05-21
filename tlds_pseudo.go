1b843e8b2d9abada3052871c092f4a5aaf8a6459
Daniel Martí <mvdan@mvdan.cc>
2015-05-21 20:00:49 +0200

    Add .gnu and .zkey as pseudo TLDs

--- tlds_pseudo.go
+++ tlds_pseudo.go
@@ -5,10 +5,13 @@ package xurls
 // Sources:
 //  * https://en.wikipedia.org/wiki/Pseudo-top-level_domain
 //  * https://en.wikipedia.org/wiki/Category:Pseudo-top-level_domains
+//  * https://tools.ietf.org/html/draft-grothoff-iesg-special-use-p2p-names-00
 var PseudoTLDs = []string{
 	`bit`,   // Namecoin
 	`exit`,  // Tor exit node
+	`gnu`,   // GNS by public key
 	`i2p`,   // I2P network
 	`local`, // Local network
 	`onion`, // Tor hidden services
+	`zkey`,  // GNS domain name
 }
