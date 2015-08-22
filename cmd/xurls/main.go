6cd7d49b2cb443d1136caceec6eec9e4af072f1a
Daniel Martí <mvdan@mvdan.cc>
2015-08-21 18:03:13 -0700

    cmd/xurls: Simplify help printing

--- cmd/xurls/main.go
+++ cmd/xurls/main.go
@@ -21,11 +21,14 @@ var (
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: xurls [-h] [files]\n\n")
-		fmt.Fprintf(os.Stderr, "If no files are given, it reads from standard input.\n\n")
-		fmt.Fprintf(os.Stderr, "   -m <regexp>   only match urls whose scheme matches a regexp\n")
-		fmt.Fprintf(os.Stderr, "                    example: 'https?://|mailto:'\n")
-		fmt.Fprintf(os.Stderr, "   -r            also match urls without a scheme (relaxed)\n")
+		p := func(format string, a ...interface{}) {
+			fmt.Fprintf(os.Stderr, format, a...)
+		}
+		p("Usage: xurls [-h] [files]\n\n")
+		p("If no files are given, it reads from standard input.\n\n")
+		p("   -m <regexp>   only match urls whose scheme matches a regexp\n")
+		p("                    example: 'https?://|mailto:'\n")
+		p("   -r            also match urls without a scheme (relaxed)\n")
 	}
 }
 
