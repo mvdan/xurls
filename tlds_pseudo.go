b39c58e79eaf4e564a68bb34ae545194988f90c2
Daniel Martí <mvdan@mvdan.cc>
2016-02-07 20:33:48 +0000

    Add missing headers to non-generated files

--- tlds_pseudo.go
+++ tlds_pseudo.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2015, Daniel Martí <mvdan@mvdan.cc>
+// See LICENSE for licensing information
+
 package xurls
 
 // PseudoTLDs is a sorted list of some widely used unofficial TLDs.
